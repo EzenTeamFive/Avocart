01c44da188b2195653f5a1984b885d72bbb16924
kimms0311 <ahd070721@gmail.com>
2023-12-26 15:19:11 +0900

    1226 commit

--- finalProject/src/main/java/com/avo/www/service/MyListService.java
+++ finalProject/src/main/java/com/avo/www/service/MyListService.java
@@ -3,6 +3,7 @@ package com.avo.www.service;
 import java.util.List;
 
 import com.avo.www.domain.CommunityBoardVO;
+import com.avo.www.domain.FileVO;
 import com.avo.www.domain.LikeItemVO;
 import com.avo.www.domain.PagingVO;
 import com.avo.www.domain.ProductBoardVO;
@@ -15,11 +16,23 @@ public interface MyListService {
 
 	List<LikeItemVO> getLikeList(String liUserId);
 
-	int getCommuTotalCount(PagingVO pgvo, String liUserId);
+//	int getCommuTotalCount(PagingVO pgvo, String liUserId);
+//
+//	List<LikeItemVO> getCommuLikeList(String liUserId);
+//
+//	List<CommunityBoardVO> getMoreCommuList(PagingVO pgvo, String liUserId);
 
-	List<LikeItemVO> getCommuLikeList(String liUserId);
+	List<CommunityBoardVO> getCommuWriteList(String userEmail);
 
-	List<CommunityBoardVO> getMoreCommuList(PagingVO pgvo, String liUserId);
+	List<FileVO> getFileList(long cmBno);
+
+	List<CommunityBoardVO> getCommuLikeList(String userEmail);
+
+	List<ProductBoardVO> likeProList(String userEmail, String type);
+
+	List<LikeItemVO> likeList(String userEmail, long proBno);
+
+	List<FileVO> getLikeFileList(long proBno);
 
 
 
--- finalProject/src/main/java/com/avo/www/service/MyListServiceImpl.java
+++ finalProject/src/main/java/com/avo/www/service/MyListServiceImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 
 import com.avo.www.domain.CommunityBoardVO;
+import com.avo.www.domain.FileVO;
 import com.avo.www.domain.LikeItemVO;
 import com.avo.www.domain.PagingVO;
 import com.avo.www.domain.ProductBoardVO;
@@ -17,8 +18,28 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 public class MyListServiceImpl implements MyListService{
+	
+	
 	@Inject
 	private MyListDAO ldao;
+
+	// 관심목록 -------------------------------------------------------
+	
+	@Override
+	public List<ProductBoardVO> likeProList(String userEmail, String type) {
+		return ldao.likeProList(userEmail,type);
+	}
+
+	@Override
+	public List<LikeItemVO> likeList(String userEmail, long proBno) {
+		return ldao.likeList(userEmail,proBno);
+	}
+
+	@Override
+	public List<FileVO> getLikeFileList(long proBno) {
+		return ldao.getLikeFileList(proBno);
+	}
+
 
 	@Override
 	public int getTotalCount(PagingVO pgvo, String liUserId) {
@@ -36,25 +57,48 @@ public class MyListServiceImpl implements MyListService{
 		return ldao.getLikeList(liUserId);
 	}
 
+	// 동네생활 -------------------------------------------------------
+	
+	//작성글
 	@Override
-	public int getCommuTotalCount(PagingVO pgvo, String liUserId) {
-		return ldao.getCommuTotalCount(pgvo,liUserId);
+	public List<CommunityBoardVO> getCommuWriteList(String userEmail) {
+		return ldao.getCommuWriteList(userEmail);
 	}
 
 	@Override
-	public List<LikeItemVO> getCommuLikeList(String liUserId) {
-		log.info("getCommuLikeList 진입 >> " );
-		log.info("liUserId >> " + liUserId);
-		return ldao.getCommuLikeList(liUserId);
+	public List<FileVO> getFileList(long cmBno) {
+		return ldao.getFileList(cmBno);
 	}
 
 	@Override
-	public List<CommunityBoardVO> getMoreCommuList(PagingVO pgvo, String liUserId) {
-		log.info("getMoreCommuList 진입 >> " );
-		List<CommunityBoardVO> list = ldao.getMoreCommuList(pgvo, liUserId);
-		log.info("list >> " , list);
-		return list;
+	public List<CommunityBoardVO> getCommuLikeList(String userEmail) {
+		return ldao.getCommuLikeList(userEmail);
 	}
+
+
+
+
+
+
+//	@Override
+//	public int getCommuTotalCount(PagingVO pgvo, String liUserId) {
+//		return ldao.getCommuTotalCount(pgvo,liUserId);
+//	}
+//
+//	@Override
+//	public List<LikeItemVO> getCommuLikeList(String liUserId) {
+//		log.info("getCommuLikeList 진입 >> " );
+//		log.info("liUserId >> " + liUserId);
+//		return ldao.getCommuLikeList(liUserId);
+//	}
+//
+//	@Override
+//	public List<CommunityBoardVO> getMoreCommuList(PagingVO pgvo, String liUserId) {
+//		log.info("getMoreCommuList 진입 >> " );
+//		List<CommunityBoardVO> list = ldao.getMoreCommuList(pgvo, liUserId);
+//		log.info("list >> " , list);
+//		return list;
+//	}
 
 
 	
